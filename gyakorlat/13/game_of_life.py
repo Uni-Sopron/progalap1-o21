b547caf6c55077c8c963af096e5099f90dabe08e
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2021-11-29 12:57:10 +0100

    Game of life outline

--- gyakorlat/13/game_of_life.py
+++ gyakorlat/13/game_of_life.py
@@ -1,7 +1,30 @@
+import json
+from typing import List
 
+def make_empty_board(width: int, height: int) -> List[List[bool]]:
+    pass
+
+def fill_board(board: List[List[bool]], percent: float) -> None:
+    pass
+
+def print_board(board: List[List[bool]], alive_char: str, dead_char: str) -> None:
+    pass
+
+def next_generation(board: List[List[bool]]) -> List[List[bool]]:
+    pass
 
 def main():
-    pass
+    config = json.load(open('config.json'))
+    width = config['width']
+    height = config['height']
+    board = make_empty_board(width, height)
+    fill_board(board, config['alive_percent_start'])
+    for round in range(config['num_rounds']):
+        print('Round', round)
+        print_board(board, config['alive'], config['dead'])
+        board = next_generation(board)
+    print('At the end:')
+    print_board(board, config['alive'], config['dead'])
 
 if __name__ == "__main__":
     main()
